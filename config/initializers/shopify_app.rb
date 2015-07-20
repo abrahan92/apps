26728ed590115a2a52ee52050d2cb98274be691c
Abrahan Mendoza <abrahanm@2becommerce.com>
2015-07-20 00:41:48 -0430

    Initial Commit

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "f15cf569b39f9a91fb81c7c4d90e5139"
+  config.secret = "2a7180da9e62cd3085db749ab68d7e9d"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
